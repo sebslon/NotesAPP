65698c457596aea94606cff71245ba5ab8897baa
Sebastian <sebastian.sloniec@gmail.com>
2021-07-27 18:24:39 +0200

    Create Header component

---
+++ src/components/Header/__tests__/Header.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import Header from "../Header";
+
+describe("<Header />", () => {
+  it("Renders <Header /> with name of the app", () => {
+    const { getByText } = render(<Header />);
+
+    expect(getByText(/Notes APP/i)).toBeInTheDocument();
+  });
+})
+
