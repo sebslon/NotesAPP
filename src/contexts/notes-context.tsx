fe9da67b2a7ffa1b0d154808d04944b3fcc24154
Sebastian <sebastian.sloniec@gmail.com>
2021-07-28 20:37:27 +0200

    feat: Implement NotesContext withlocal state and reducer

---
+++ src/contexts/notes-context.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useReducer } from "react";
+
+interface Note {
+    id: number,
+    text: string,
+    date: string,
+}
+
+type ActionType = 
+    | { type: "ADD_NOTE", payload: Note }
+    | { type: "DELETE_NOTE", payload: { id: number } }
+
+const initialState: Note[] = [];
+
+export const NotesContext = createContext<{
+    state: Note[];
+    dispatch: React.Dispatch<ActionType>;
+}>({
+    state: initialState,
+    dispatch: () => null
+})
+
+const reducer = (state: Note[], action: ActionType) => {
+    switch (action.type) {
+        case "ADD_NOTE":
+            return [...state, action.payload];
+        case "DELETE_NOTE":
+            return state.filter(note => note.id !== action.payload.id)
+        default:
+            return state;
+    }
+}
+
+export const NotesContextProvider: React.FC = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return <NotesContext.Provider value={{state, dispatch}}>{children}</NotesContext.Provider>
+};
