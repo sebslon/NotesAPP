df905868c1abf636f8a79b799df274bf6ab3e5b0
Sebastian <sebastian.sloniec@gmail.com>
2021-07-28 20:43:06 +0200

    feat: Note component

---
+++ src/Note/Note.tsx
@@ -0,0 +1,31 @@
+import './Note.css';
+import { FormEvent, useContext } from 'react';
+
+import { NotesContext } from '../contexts/notes-context';
+
+interface NoteProps {
+    text: string;
+    date: string;
+    id: number;
+}
+
+export default function Note({ text, date, id }: NoteProps) {
+    const { dispatch } = useContext(NotesContext);
+
+    const handleClick = (e: FormEvent) => {
+        e.preventDefault();
+
+        dispatch({
+            type: "DELETE_NOTE",
+            payload: { id }
+        })
+    };
+
+    return (
+        <div className="note">
+            <p>{text}</p>
+            <button onClick={handleClick}>Delete note</button>
+            <span className="note__date">{date}</span>
+        </div>
+    );
+};
